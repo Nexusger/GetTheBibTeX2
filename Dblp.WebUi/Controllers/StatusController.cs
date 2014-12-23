ac0da76dfd6485885ea7450a469446fd4b51080e
Torben Dohrn <github.com@nexusger.de>
2014-12-23 13:34:45 +0100

    Implemented status, now takes data from repo

--- Dblp.WebUi/Controllers/StatusController.cs
+++ Dblp.WebUi/Controllers/StatusController.cs
@@ -1,18 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Dblp.Domain.Abstract;
 using Dblp.WebUi.Models;
 
 namespace Dblp.WebUi.Controllers
 {
     public class StatusController : Controller
     {
+
+        private IDblpRepository _repo;
+
+        public StatusController(IDblpRepository repo)
+        {
+            _repo = repo;
+        }
         // GET: Status
         public ActionResult Index()
         {
-            var model = new StatusViewModel(new DateTime(2004, 6, 8), 15, 100);
+
+            var numberOfAuthors = _repo.People.Count();
+            var numberOfPublications = _repo.Proceedings.Count();
+
+
+            var model = new StatusViewModel(DateTime.UtcNow, numberOfAuthors, 0,numberOfPublications);
             
             return View(model);
         }
--- Dblp.WebUi/Models/StatusViewModel.cs
+++ Dblp.WebUi/Models/StatusViewModel.cs
@@ -5,15 +5,18 @@ namespace Dblp.WebUi.Models
     public class StatusViewModel
 
     {
-        public StatusViewModel(DateTime lastUpdated, long numberOfAuthors, long numberOfConferences)
+        public StatusViewModel(DateTime lastUpdated, long numberOfAuthors, long numberOfConferences, long numberOfPublications)
         {
             LastUpdated = lastUpdated;
             NumberOfAuthors = numberOfAuthors;
             NumberOfConferences = numberOfConferences;
+            NumberOfPublications = numberOfPublications;
         }
         public DateTime LastUpdated { get; private set; }
         public long NumberOfAuthors { get; private set; }
-        public long NumberOfConferences { get; set; }
+        public long NumberOfConferences { get; private set; }
+
+        public long NumberOfPublications { get; private set; }
 
     }
 }
