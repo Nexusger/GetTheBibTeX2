44ed351172d3e1d8462586b2b5ba0e6745022a5a
Torben Dohrn <github.com@nexusger.de>
2014-12-23 14:04:09 +0100

    changed MenuItemWithGlyphicon to overload of MenuItem, Added InputWithGlyphicon

--- Dblp.WebUi/Helpers/MenuExtension.cs
+++ Dblp.WebUi/Helpers/MenuExtension.cs
@@ -31,8 +31,9 @@ namespace Dblp.WebUi.Helpers
 
         /// <summary>
         /// Source http://stackoverflow.com/questions/6323021/better-way-to-get-active-page-link-in-mvc-3-razor
+        /// Source http://forums.asp.net/p/1702210/4518688.aspx?Re+Quick+question+about+Ajax+ActionLink+and+span
         /// </summary>
-        public static MvcHtmlString MenuItemWithGlyphIcon(
+        public static MvcHtmlString MenuItem(
             this HtmlHelper htmlHelper,
             string text,
             string action,
@@ -41,11 +42,7 @@ namespace Dblp.WebUi.Helpers
             )
         {
 
-            if (!String.IsNullOrEmpty(glyhicon))
-            {
-                var span = new TagBuilder("span");
-                span.AddCssClass(glyhicon);
-            }
+          
             var li = new TagBuilder("li");
             var routeData = htmlHelper.ViewContext.RouteData;
             var currentAction = routeData.GetRequiredString("action");
@@ -55,19 +52,38 @@ namespace Dblp.WebUi.Helpers
             {
                 li.AddCssClass("active");
             }
-            if (!String.IsNullOrEmpty(glyhicon))
-            {
-                var span = new TagBuilder("span");
-                span.AddCssClass(glyhicon);
-                li.InnerHtml = span + htmlHelper.ActionLink(text, action, controller).ToHtmlString();
-            }
-            else
-            {
-                li.InnerHtml = htmlHelper.ActionLink(text, action, controller).ToHtmlString();
 
-            }
-            return MvcHtmlString.Create(li.ToString());
+            var span = new TagBuilder("span");
+            span.AddCssClass("glyphicon " + glyhicon);
+            span.Attributes.Add("aria-hidden", "true");
+            var t = Guid.NewGuid().ToString();
+            li.InnerHtml = htmlHelper.ActionLink(t, action, controller).ToHtmlString();
+            
+            return MvcHtmlString.Create(li.ToString().Replace(t,span+" "+text));
         }
 
+        public static MvcHtmlString InputWithGlyphicon(
+            this HtmlHelper htmlHelper)
+        {
+            var input = new TagBuilder("input");
+            input.Attributes.Add("type","text");
+            input.Attributes.Add("placeholder", "LAK, Romero, Analytics,...");
+            input.Attributes.Add("autofocus","");
+            input.AddCssClass("form-control");
+            input.AddCssClass("typeahead");
+            var innerSpan = new TagBuilder("span");
+            innerSpan.AddCssClass("glyphicon-search");
+            innerSpan.AddCssClass("glyphicon");
+            var outerSpan = new TagBuilder("span");
+            outerSpan.AddCssClass("input-group-addon");
+            var div = new TagBuilder("div");
+            div.AddCssClass("input-group-lg");
+            div.AddCssClass("input-group");
+            div.Attributes.Add("id","search");
+            outerSpan.InnerHtml = innerSpan.ToString();
+            div.InnerHtml = outerSpan.ToString() +" "+ input;
+
+            return MvcHtmlString.Create(div.ToString());
+        }
     }
 }
