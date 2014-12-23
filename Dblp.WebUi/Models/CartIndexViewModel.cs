bac048b14a170ff9b38019f091ac3731f9b1978c
Torben Dohrn <github.com@nexusger.de>
2014-12-23 17:04:43 +0100

    MInor: Formatting

--- Dblp.WebUi/Models/CartIndexViewModel.cs
+++ Dblp.WebUi/Models/CartIndexViewModel.cs
@@ -3,8 +3,8 @@
 namespace Dblp.WebUi.Models
 {
     public class CartIndexViewModel
-    { public ShoppingCart Cart { get; set; } 
-        public string ReturnUrl 
-        { get; set; } 
+    {
+        public ShoppingCart Cart { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }
