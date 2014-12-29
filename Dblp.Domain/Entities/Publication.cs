356cbaa82e4e5e62a4ba9df6f152c930730ba84a
Torben Dohrn <github.com@nexusger.de>
2014-12-29 23:06:05 +0100

    Added entities for conferences

---
+++ Dblp.Domain/Entities/Publication.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Dblp.Domain.Entities
+{
+    [DataContract]
+    public class Publication
+    {
+        [Key]
+        [DataMember]
+        public string Key { get; set; }
+        [DataMember]
+        public List<string> Authors { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+    }
+}
---
+++ Dblp.Domain/Entities/SubConference.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Dblp.Domain.Entities
+{
+    [DataContract]
+    public class SubConference
+    {
+        [Key]
+        [DataMember]
+        public string Key { get; set; }
+        
+        [DataMember]
+        public string Title { get; set; }
+        
+        [DataMember]
+        public List<Publication> Publications { get; set; }
+        public override string ToString()
+        {
+            return Key + " " + Title;
+        }
+    }
+}
