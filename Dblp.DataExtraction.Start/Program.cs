d33aa6fbe073989ddc3589bb5ce35149d8405218
Torben Dohrn <github.com@nexusger.de>
2015-01-31 16:34:33 +0100

    Changed folder name of startup project

---
+++ Dblp.DataExtraction.Start/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Dblp.ExtractBht;
+using Dblp.ExtractXml;
+
+namespace Start
+{
+    public class Program
+    {
+        public const string Startpath = @"D:\dblp\bht\db\conf\";
+        static void Main(string[] args)
+        {
+            var extractor = new StructureExtractor();
+            extractor.StartScan(Startpath);
+
+            var keyTitleLookUp = new KeyTitleLookUp(@"D:\dblp\dblp.xml");
+
+            Parallel.ForEach(extractor.Structures, conference =>
+            {
+                foreach (var @event in conference.Events)
+                {
+                    if(@event.Publications!=null)
+                    foreach (var publication in @event.Publications)
+                    {
+                        if (publication != null)
+                        {
+                            if (keyTitleLookUp.SmallSearchResults.ContainsKey(publication.Key))
+                            {
+                                publication.Title = keyTitleLookUp.SmallSearchResults[publication.Key];
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
