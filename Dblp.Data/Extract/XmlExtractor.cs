c2136b6f82980d907681e497de2a4769b88cba45
Torben Dohrn <github.com@nexusger.de>
2015-02-27 21:15:42 +0100

    Added Extract of bibtex Files

---
+++ Dblp.Data/Extract/XmlExtractor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using Dblp.Helper;
+
+namespace Dblp.Data.Extract
+{
+    public class XmlExtractor
+    {
+        public string InputFileName { get; set; }
+        public string OutputFileName { get; set; }
+
+        public Dictionary<string, string> SmallSearchResults = new Dictionary<string, string>();
+        public XmlExtractor(string inputFileName)
+        {
+            InternRead(inputFileName);
+        }
+        public static IEnumerable<KeyValuePair<string, string>> Read(string inputFileName)
+        {
+
+            var setting = new XmlReaderSettings {DtdProcessing = DtdProcessing.Parse};
+            using (XmlReader reader = XmlReader.Create(File.OpenText(inputFileName), setting))
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element && reader.Name != "dblp")
+                    {
+
+                        foreach (var x in XmlHelper.GetChildContentElements(reader))
+                        {
+                            var key = x.Attribute("key").Value;
+                            yield return new KeyValuePair<string, string>(key, x.ToString());
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private void InternRead(string inputFileName)
+        {
+            var i = 0;
+            foreach (var keyValuePair in Read(inputFileName))
+            {
+                SmallSearchResults.Add(keyValuePair.Key,keyValuePair.Value);
+                i++;
+                if (i % 10000 == 0)
+                    Trace.TraceInformation("10-Tausend ({0})", i);
+            }
+        }
+
+    }
+}
--- Dblp.DataExtraction.Start/Program.cs
+++ Dblp.DataExtraction.Start/Program.cs
@@ -1,8 +1,10 @@
-﻿using System.Configuration;
-using System.Data.Entity.Infrastructure.Design;
-using System.Net.Mime;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Dblp.Data;
+using Dblp.Data.Extract;
 using Dblp.Data.Saver;
+using Dblp.Helper;
 
 namespace Dblp.DataExtraction.Start
 {
@@ -12,10 +14,19 @@ namespace Dblp.DataExtraction.Start
         public const string PathToBhtFolder = @"D:\dblp\bht\db\conf\";
         static void Main(string[] args)
         {
-            var repo = new InMemoryDataStore(PathToDblpXml, PathToBhtFolder);
-            var saver = new EntityFrameWorkSaver(Settings.Default.BatchSize);
-            saver.SaveConferences(repo.Conferences);
-            
+            //var repo = new InMemoryDataStore(PathToDblpXml, PathToBhtFolder);
+            //var saver = new EntityFrameWorkSaver(Settings.Default.BatchSize);
+            //saver.SaveConferences(repo.Conferences);
+            //repo = null;
+            //saver = null;
+
+            var x = new XmlExtractor(@"D:\dblp\dblp-min.xml");
+            var a = new List<string>();
+            foreach (var y in x.SmallSearchResults.Where(t=>(!t.Key.Contains("persons")) && (!t.Key.Contains("www"))))
+            {
+                a.Add(y.Value.ToBibTeX());
+            }
+            File.WriteAllLines(@"c:\temp\bibtex2.bib",a);
         }
     }
 }
