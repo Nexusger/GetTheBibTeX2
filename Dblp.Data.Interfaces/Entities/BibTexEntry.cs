dc3452ce0404fcda693acffe43c4a6312ac26513
Torben Dohrn <github.com@nexusger.de>
2015-03-03 23:19:24 +0100

    Added entities bibtex entry and loadDetails;

---
+++ Dblp.Data.Interfaces/Entities/BibTexEntry.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Dblp.Data.Interfaces.Entities
+{
+    
+    [DataContract]
+    public class BibTexEntry
+    {
+        [Key]
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        [MaxLength(50)]
+        public string Key { get; set; }
+
+        [DataMember]
+        public string Content { get; set; }
+
+    }
+}
---
+++ Dblp.Data.Interfaces/Entities/LoadDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Dblp.Data.Interfaces.Entities
+{
+    [DataContract]
+    public class LoadDetails
+    {
+        [Key]
+        [DataMember]
+        public long Key { get; set; }
+        [DataMember]
+        public TimeSpan LoadTime { get; set; }
+        [DataMember]
+        public DateTime LastLoaded { get; set; }
+
+        [DataMember]
+        public long AmountBhtFilesParsed { get; set; }
+        [DataMember]
+        public long SizeOfXml { get; set; }
+    }
+}
