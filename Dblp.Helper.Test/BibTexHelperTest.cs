65c9a6039bcfbc3a46f63eb1075191d2aeb9cb94
Torben Dohrn <github.com@nexusger.de>
2015-03-14 18:05:06 +0100

    Imploded Authors / Multiple Nodes #13

---
+++ Dblp.Helper.Test/BibTexHelperTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dblp.Helper.Test
+{
+    [TestClass]
+    public class BibTexHelperTest
+    {
+
+        private static string GetXElement()
+        {
+            return "<article mdate=\"2009-05-26\" key=\"journals/or/GrigoroudisS03\"><author>Evangelos Grigoroudis</author><author>O. Spyridaki</author><title>Derived vs. stated importance in customer satisfaction surveys.</title><pages>229-247</pages><year>2003</year><volume>3</volume><journal>Operational Research</journal><number>3</number><ee>http://dx.doi.org/10.1007/BF02936403</ee><url>db/journals/or/or3.html#GrigoroudisS03</url></article>";
+        }
+        [TestMethod]
+        public void ToBibTexTest()
+        {
+            //Arrange
+            
+            //Act
+            var target = GetXElement().ToBibTeX();
+            //Assert
+            Assert.IsTrue(target.Contains("@article{"),"Type is mising");
+            Assert.IsTrue(target.Contains("author = {Evangelos Grigoroudis and O. Spyridaki}"),"Author ist falsch");
+            Assert.IsTrue(target.Contains("title = {Derived vs. stated importance in customer satisfaction surveys.}"), "title ist falsch");
+            Assert.IsTrue(target.Contains("journal = {Operational Research}"), "journal ist falsch");
+            Assert.IsTrue(target.Contains("volume = {3}"), "volume ist falsch");
+            Assert.IsTrue(target.Contains("number = {3}"),"number ist falsch");
+            Assert.IsTrue(target.Contains("pages = {229-247}"),"pages ist falsch");
+            Assert.IsTrue(target.Contains("year = {2003}"),"year ist falsch");
+            Assert.IsTrue(target.Contains("url = {http://dx.doi.org/10.1007/BF02936403}"),"url ist falsch");
+        }
+
+        [TestMethod]
+        public void SpecialCharacterTest_GivenOUmlaut_returnEncodedBibTexEntry()
+        {
+            //Arrange
+            var xml =
+                "<article mdate=\"2009-05-26\" key=\"journals/or/GrigoroudisS03\"><author>abcdeöÜä</author></article>";
+            //Act
+            var target = xml.ToBibTeX();
+            //Assert
+            Assert.IsTrue(target.Contains("abcde{\"{o}}{\"{U}}{\"{a}}"));
+        }
+    }
+}
--- Dblp.Helper/BibTexHelper.cs
+++ Dblp.Helper/BibTexHelper.cs
@@ -1,4 +1,7 @@
-﻿using System.Security;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Policy;
 using System.Xml.Linq;
 
 namespace Dblp.Helper
@@ -11,21 +14,76 @@ namespace Dblp.Helper
             var x = XElement.Parse(xElement);
             var attributeName = x.Name;
             result = string.Format("@{0}{{{1},\r\n", attributeName, x.GetKey());
+            var singleNodes = new List<XNode>();
+            var multipleNodes = new List<XNode>();
+            foreach (var xNode in x.Nodes())
+            {
+                if (!singleNodes.Any(t => (t as XElement).Name == (xNode as XElement).Name))
+                {
+                    singleNodes.Add(xNode);
+                }
+                else
+                {
+                    multipleNodes.Add(singleNodes.FirstOrDefault(t => (t as XElement).Name == (xNode as XElement).Name));
+                    singleNodes.Remove(singleNodes.FirstOrDefault(t => (t as XElement).Name == (xNode as XElement).Name));
+                    multipleNodes.Add(xNode);
+                }
+            }
+
             foreach (var node in x.Nodes())
             {
-                result+= node.ToBibTeXAtribute();
+                if (singleNodes.Any(t => (t as XElement).Name == (node as XElement).Name))
+                {
+                    result += node.ToBibTeXAtribute();
+                }
+                else
+                {
+                    result += MultiNodeAttributesToBibTex(multipleNodes, node);
+                }
+            }
+
+
+
+            result += "}";
+
+            return result;
+        }
+
+        private static string MultiNodeAttributesToBibTex(List<XNode> multipleNodes, XNode node)
+        {
+
+            var attributeName = (node as XElement).Name.ToString();
+            var result
+            = attributeName+=" = {";
+            result += (multipleNodes.FirstOrDefault() as XElement).Value;
+            foreach (var multipleNode in multipleNodes.Where(t => (t as XElement).Name == (node as XElement).Name).Skip(1))
+            {
+                var attributeValue = (multipleNode as XElement).Value;
+                result += " and " + attributeValue;
             }
             result += "}";
+
+
 
             return result;
         }
 
         private static string ToBibTeXAtribute(this XNode xNode)
         {
-            var attributeName = (xNode as XElement).Name;
+            var attributeName = ExtractAttributeName(xNode);
             var attributeValue = (xNode as XElement).Value;
             var result = string.Format("  {0} = {{{1}}},\r\n", attributeName, attributeValue);
             return result;
+        }
+
+        private static XName ExtractAttributeName(XNode xNode)
+        {
+            var attributeName = (xNode as XElement).Name;
+            if (attributeName == "ee")
+            {
+                return "url";
+            }
+            return attributeName;
         }
 
         public static string GetKey(this XElement xElement)
