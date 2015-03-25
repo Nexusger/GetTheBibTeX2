f09e9d6974f3bbca08ffbb6ec2956225ba91e7f6
Torben Dohrn <github.com@nexusger.de>
2015-03-25 02:17:20 +0100

    Changes for prodution

--- Dblp.WebUi/Controllers/ShoppingCartController.cs
+++ Dblp.WebUi/Controllers/ShoppingCartController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Web.Mvc;
 using Dblp.Domain;
 using Dblp.Domain.Interfaces;
 using Dblp.Domain.Interfaces.Entities;
 using Dblp.WebUi.Models;
+using Ninject.Infrastructure.Language;
 
 namespace Dblp.WebUi.Controllers
 {
@@ -22,25 +24,16 @@ namespace Dblp.WebUi.Controllers
         public void AddKey(string dblpKey)
         {
             SearchResult searchResult;
-            if (IsConferenceKey(dblpKey))
-            {
-                if (_repo.ConferenceExists(dblpKey))
-                {
-                    searchResult = _repo.GetConferenceEventsByKeyAsSearchResult(dblpKey);
-                }
-                else
-                {
-                    return;
-                }
-            }
-            else
+            searchResult = _repo.GetConferenceEventsByKeyAsSearchResult(dblpKey);
+
+            if (searchResult == null)
             {
                 if (!_repo.PublicationExists(dblpKey))
                     return;
                 searchResult = _repo.GetPublicationByKeyAsSearchResult(dblpKey);
             }
-            if (searchResult != null)
-                GetCart().AddItem(searchResult);
+
+            GetCart().AddItem(searchResult);
         }
 
         public void DeleteKey(string dblpKey)
@@ -86,7 +79,8 @@ namespace Dblp.WebUi.Controllers
 
             }
             extendedSearchResults.AddRange(GetCart().SearchResults);
-            byte[] fileBytes = _bibTeXContentProvider.GetBibTexFileBytes(extendedSearchResults, BibTeXContentOptions.None);
+            
+            byte[] fileBytes = _bibTeXContentProvider.GetBibTexFileBytes(extendedSearchResults.Distinct(), BibTeXContentOptions.None);
             const string fileName = "myfile.bib";
             return File(fileBytes, "text/x-bibtex", fileName);
         }
