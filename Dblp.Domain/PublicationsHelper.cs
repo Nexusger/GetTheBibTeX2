98c57e24ea8233224957b51ade5e40051f57fed0
Torben Dohrn <github.com@nexusger.de>
2015-03-28 21:09:57 +0100

    Removed not needed references

--- Dblp.Domain/PublicationsHelper.cs
+++ Dblp.Domain/PublicationsHelper.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.WebPages.Scope;
 using Dblp.Domain.Interfaces.Entities;
 
 namespace Dblp.Domain
